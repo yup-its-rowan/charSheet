1da00154cd376a74dbfd72f5fb4748876194c440
Rohan Akki <roha.akki@gmail.com>
2023-04-10 01:57:34 -0400

    trying to get rid of certain files

---
+++ public/index.js
@@ -0,0 +1,45 @@
+
+var playerName = null;
+
+function LoadPlayerData(){
+    refreshACHP();
+    refreshStats();
+    refreshClassAndName();
+    refreshCoin();
+    refreshNotesAndBackstory();
+    refreshSpells();
+    refreshAbilities();
+    refreshAttacks();
+}
+
+function refreshSpells(){
+
+}
+
+function refreshAbilities(){
+
+}
+
+function refreshAttacks(){
+
+}
+
+function refreshACHP(){
+
+}
+
+function refreshStats(){
+
+}
+
+function refreshClassAndName(){
+
+}
+
+function refreshCoin(){
+
+}
+
+function refreshNotesAndBackstory(){
+
+}
