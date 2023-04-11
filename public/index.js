41805b644b76048ac43975c51d46b7ef3fd9757b
Rohan Akki <roha.akki@gmail.com>
2023-04-10 20:11:45 -0400

    open new greatsword

--- public/index.js
+++ public/index.js
@@ -1,45 +1,78 @@
 
 var playerName = null;
 
+function attackModal(){
+    
+}
+
+
+
 function LoadPlayerData(){
-    refreshACHP();
-    refreshStats();
-    refreshClassAndName();
-    refreshCoin();
-    refreshNotesAndBackstory();
-    refreshSpells();
-    refreshAbilities();
-    refreshAttacks();
+    updateACHP();
+    updateStats();
+    updateClassAndName();
+    updateCoin();
+    updateNotesAndBackstory();
+    updateSpells();
+    updateAbilities();
+    updateAttacks();
 }
 
-function refreshSpells(){
-
-}
-
-function refreshAbilities(){
+function updateSpells(){
 
 }
 
-function refreshAttacks(){
+function updateAbilities(){
 
 }
 
-function refreshACHP(){
+function updateAttacks(){
 
 }
 
-function refreshStats(){
+function updateACHP(){
 
 }
 
-function refreshClassAndName(){
+function updateStats(){
 
 }
 
-function refreshCoin(){
+function updateClassAndName(){
 
 }
 
-function refreshNotesAndBackstory(){
+function updateCoin(){
 
+}
+
+function updateNotesAndBackstory(){
+
+}
+
+function attackButton() {
+    var elem = document.getElementsByClassName("writtenAttackList")[0];
+    var writtenAttack1 = document.createElement("div");
+    writtenAttack1.className = "writtenAttack centerer";
+    var weapon = document.createElement("p");
+    var attackRollMod = document.createElement("p");
+    var damageRoll = document.createElement("p");
+    var damageType = document.createElement("p");
+    var additionalInfo = document.createElement("p");
+    weapon.className = "weapon";
+    weapon.innerHTML = "Greatsword";
+    attackRollMod.className = "attackRollMod";
+    attackRollMod.innerHTML = "+4";
+    damageRoll.className = "damageRoll";
+    damageRoll.innerHTML = "2d6";
+    damageType.className = "damageType";
+    damageType.innerHTML = "Slashing";
+    additionalInfo.className = "additionalInfo";
+    additionalInfo.innerHTML = "Looks Pretty";
+    writtenAttack1.appendChild(weapon);
+    writtenAttack1.appendChild(attackRollMod);
+    writtenAttack1.appendChild(damageRoll);
+    writtenAttack1.appendChild(damageType);
+    writtenAttack1.appendChild(additionalInfo);
+    elem.appendChild(writtenAttack1);
 }
