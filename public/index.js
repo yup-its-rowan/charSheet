2e0007cf154825282f8b1cfcf5a35720552c15eb
Rohan Akki <roha.akki@gmail.com>
2023-04-11 00:56:46 -0400

    first adding modal

--- public/index.js
+++ public/index.js
@@ -50,7 +50,7 @@ function updateNotesAndBackstory(){
 
 }
 
-function attackButton() {
+function attackButton(weapon1, attackRollMod1, damageRoll1, damageType1, additionalInfo1) {
     var elem = document.getElementsByClassName("writtenAttackList")[0];
     var writtenAttack1 = document.createElement("div");
     writtenAttack1.className = "writtenAttack centerer";
@@ -60,19 +60,28 @@ function attackButton() {
     var damageType = document.createElement("p");
     var additionalInfo = document.createElement("p");
     weapon.className = "weapon";
-    weapon.innerHTML = "Greatsword";
+    weapon.innerHTML = weapon1;
     attackRollMod.className = "attackRollMod";
-    attackRollMod.innerHTML = "+4";
+    attackRollMod.innerHTML = attackRollMod1;
     damageRoll.className = "damageRoll";
-    damageRoll.innerHTML = "2d6";
+    damageRoll.innerHTML =  damageRoll1;
     damageType.className = "damageType";
-    damageType.innerHTML = "Slashing";
+    damageType.innerHTML =  damageType1;
     additionalInfo.className = "additionalInfo";
-    additionalInfo.innerHTML = "Looks Pretty";
+    additionalInfo.innerHTML =  additionalInfo1;
     writtenAttack1.appendChild(weapon);
     writtenAttack1.appendChild(attackRollMod);
     writtenAttack1.appendChild(damageRoll);
     writtenAttack1.appendChild(damageType);
     writtenAttack1.appendChild(additionalInfo);
     elem.appendChild(writtenAttack1);
+}
+
+//make sure to keep updating this below
+window.onclick = function(event) {
+    newAttackModal = document.getElementById("newAttackModal");
+    if (event.target == newAttackModal) {
+        attackButton(newAttackModal.getElementsByClassName("nameInput")[0].value, newAttackModal.getElementsByClassName("attackBonusInput")[0].value, newAttackModal.getElementsByClassName("damageInput")[0].value, newAttackModal.getElementsByClassName("damageTypeInput")[0].value, newAttackModal.getElementsByClassName("additionalInfoInput")[0].value);
+        newAttackModal.style.display = "none";
+    }
 }
